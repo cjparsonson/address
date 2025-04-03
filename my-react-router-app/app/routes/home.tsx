7a794d2b92d53bd22bbe206ff975903e026a92dd
cjparsonson <cjparsonson@gmail.com>
2025-04-03 16:56:45 +0100

    First Commit

---
+++ my-react-router-app/app/routes/home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <p id="index-page">
+            This is a demo for react router.
+            <br />
+            Check out{" "}
+            <a href="https://reactrouter.com">
+                the docs at reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
